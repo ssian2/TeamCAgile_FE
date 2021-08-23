8bce77968705c4cee71c3558694a436576c8f52c
Aimee Boyle <aimee.boyle@kainos.com>
2021-08-23 15:03:00 +0100

    Add job roles page and basic routing test

--- routes/sampleRoutes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var router = express.Router()
-
-
-router.get("/oops", async (req, res) => {
-    // Use service here to get data from database
-});
-
-module.exports = router
--- tests/job_role_routes.test.js
+++ tests/job_role_routes.test.js
@@ -1,10 +1,10 @@
 const request = require('supertest')
 const app = require('../app')
 
-describe('testing the home path', () => {
+describe('testing the job role path', () => {
   it('should get 200 status code', async () => {
     const res = await request(app)
-      .get('/')
+      .get('/job-roles')
     expect(res.statusCode).toEqual(200)
   })
 })
