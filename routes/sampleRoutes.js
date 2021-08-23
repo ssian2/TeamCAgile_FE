3c7f309a4798493af666b99fbba19634f3af6d5e
Aimee Boyle <aimee.boyle@kainos.com>
2021-08-23 13:29:48 +0100

    Add project structure

---
+++ routes/sampleRoutes.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var router = express.Router()
+
+
+router.get("/oops", async (req, res) => {
+    // Use service here to get data from database
+});
+
+module.exports = router
