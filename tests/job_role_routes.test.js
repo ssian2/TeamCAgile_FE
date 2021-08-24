63451e063c39432397f2d8056bdbbffe30762c78
ymtyroni <lukasz.calka@protonmail.com>
2021-08-24 16:45:45 +0200

    add an and base for selenium

--- tests/job_role_routes.test.js
+++ tests/job_role_routes.test.js
@@ -1,6 +1,14 @@
 const request = require('supertest')
 const app = require('../app')
 
+const webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+const driver = new webdriver.Builder()
+    .forBrowser('safari')
+    .build();
+
 describe('testing the job role path', () => {
   it('should get 200 status code', async () => {
     const res = await request(app)
@@ -8,4 +16,6 @@ describe('testing the job role path', () => {
     expect(res.statusCode).toEqual(200)
   })
 })
+
+
 
