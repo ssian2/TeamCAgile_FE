05fed73811ef055a16c25a7ef3a382bf98f2598a
ymtyroni <lukasz.calka@protonmail.com>
2021-08-24 15:13:31 +0200

    working front end but without testing yet

--- tests/job_role_routes.test.js
+++ tests/job_role_routes.test.js
@@ -2,17 +2,11 @@ const request = require('supertest')
 const app = require('../app')
 
 describe('testing the job role path', () => {
-  it('should get 200 status code', async () => {
+  it('should get test role name as first result', async () => {
     const res = await request(app)
-      .get('/job-roles')
-    expect(res.statusCode).toEqual(200)
+      .get('/job-roles/by-band')
+      console.log(res.body)
+    expect(res.body.role.name).toEqual("test role")
   })
 })
 
-describe('testing the job spec path', () => {
-  it('should get 200 status code', async () => {
-    const res = await request(app)
-      .get('/job-roles-spec')
-    expect(res.statusCode).toEqual(200)
-  })
-})
