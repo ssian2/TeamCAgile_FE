fe6287dc45b48568154c63268cd11535888a6d4f
Aimee Boyle <aimee.boyle@kainos.com>
2021-08-26 12:28:30 +0100

    Add view job spec functionality

--- tests/job_role_routes.test.js
+++ tests/job_role_routes.test.js
@@ -1,24 +1,6 @@
 const request = require('supertest')
 const app = require('../app.js')
 
-
-const getElementXpath = async (driver, xpath, timeout = 3000) => {
-  const el = await driver.wait(until.elementLocated(By.xpath(xpath)), timeout);
-  return await driver.wait(until.elementIsVisible(el), timeout);
-};
- 
- 
-const getElementName = async (driver, name, timeout = 3000) => {
-  const el = await driver.wait(until.elementLocated(By.name(name)), timeout);
-  return await driver.wait(until.elementIsVisible(el), timeout);
-};
- 
-const getElementId = async (driver, id, timeout = 3000) => {
-  const el = await driver.wait(until.elementLocated(By.id(id)), timeout);
-  return await driver.wait(until.elementIsVisible(el), timeout);
-};
-
-
 describe('testing the job role path', () => {
   it('should get 200 status code', async () => {
     const res = await request(app)
