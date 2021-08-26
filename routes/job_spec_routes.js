b5694ed5c5866a5c57faff6319c1ed12cc6cd369
Tasnya <tasnya.khanom@kainos.com>
2021-08-26 09:41:06 +0100

    create ui for job spec

--- routes/job_spec_routes.js
+++ routes/job_spec_routes.js
@@ -2,13 +2,22 @@ var express = require('express')
 const fetch = require("node-fetch");
 var router = express.Router()
 
+//displays specification for a particular role 
+router.get("/:specid", async (req, res) => {
 
-router.get("/", async (req, res) => {
-
-    const url='https://mocki.io/v1/cdd1fc06-7d5a-45b8-aac5-283b7bad4b46';
+    const url='http://localhost:8080/api/job-role/view-job-spec/' + req.params.specid;
     fetch(url)
     .then(data => { return data.json()})
     .then(jobspec_data => {res.render('job_roles_spec', {job_roles_spec: jobspec_data})});
+});
+
+//displays all roles with their specs 
+router.get("/", async (req, res) => {
+
+    const url='http://localhost:8080/api/job-role/view-job-spec/';
+    fetch(url)
+    .then(data => { return data.json()})
+    .then(alljobspec_data => {res.render('all_job_specs', {all_job_specs_data: alljobspec_data})});
 });
 
 module.exports = router;
