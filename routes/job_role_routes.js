cb0fff392b02122a5e69fcfcf35d4388427e6e75
Tasnya <tasnya.khanom@kainos.com>
2021-08-26 15:01:37 +0100

    create capability ui

--- routes/job_role_routes.js
+++ routes/job_role_routes.js
@@ -4,24 +4,11 @@ var router = express.Router()
 
 
 router.get("/", async (req, res) => {
-    try {
-        const url = 'http://localhost:8080/api/job-role/all';
-        await fetch(url)
-            .then(data => { return data.json() })
-            .then(jobrole_data => { res.render('job_roles', { job_roles: jobrole_data }) });
-    }
-    catch (err) {
-        res.render('job_roles', { job_roles: "" });
-    }
-});
 
-
-router.get("/by-band", async (req, res) => {
-
-    const url='http://localhost:8080/api/job-role/view-band-level';
+    const url='http://localhost:8080/api/job-role/all';
     fetch(url)
     .then(data => { return data.json()})
-    .then(jobrole_data => {res.render('job_roles_by_band', {job_roles: jobrole_data})});
+    .then(jobrole_data => {res.render('job_roles', {job_roles: jobrole_data})});
 });
 
 
