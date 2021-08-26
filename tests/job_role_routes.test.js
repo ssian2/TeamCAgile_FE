fb67c97049c6bbed99570373b23d5d77bd08bf8c
Aimee Boyle <aimee.boyle@kainos.com>
2021-08-26 13:26:56 +0100

    Add url functionality
    Test updates

--- tests/job_role_routes.test.js
+++ tests/job_role_routes.test.js
@@ -1,20 +1,23 @@
 const request = require('supertest')
 const app = require('../app.js')
 
-describe('testing the job role path', () => {
+describe('Testing the job role path', () => {
   it('should get 200 status code', async () => {
     const res = await request(app)
-    .get('/job-roles')
+      .get('/job-roles')
     expect(res.statusCode).toEqual(200)
   })
 });
 
-describe('testing the job spec path', () => {
+describe('Testing the job family path', () => {
   it('should get 200 status code', async () => {
     const res = await request(app)
-    .get('/capabilities/job-family')
+      .get('/capabilities/job-family')
     expect(res.statusCode).toEqual(200)
   })
 });
 
-afterAll(async () => { console.log('closing...'); await app.close(); });
+afterAll(async () => { 
+  console.log('closing...'); 
+  await app.close(); 
+});
