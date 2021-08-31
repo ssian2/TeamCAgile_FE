237fc40ae3f1e4e322c000d074db77359987d28f
ssian2 <simran.sian@kainos.com>
2021-08-31 15:59:58 +0100

    implement admin FE and add job role

--- routes/capability_routes.js
+++ routes/capability_routes.js
@@ -2,15 +2,13 @@ var express = require('express')
 const fetch = require("node-fetch");
 var router = express.Router()
 
-
 router.get("/job-family", async (req, res) => {
     try {
-    const url='http://localhost:8080/api/job-family/all';
-    await fetch(url)
-    .then(data => { return data.json()})
-    .then(job_families_data => {res.render('job_family', {job_families: job_families_data})});
-    }
-    catch(err){
+        const url='http://localhost:8080/api/job-family/all';
+        await fetch(url)
+        .then(data => { return data.json()})
+        .then(job_families_data => {res.render('job_family', {job_families: job_families_data})});
+    } catch(err) {
         res.render('job_family',{job_families: ""});
     }
 });
